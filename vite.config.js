74dcae1c5607b9a3bb86b113c15b9cf5253a48d5
shshouse <1839192968@qq.com>
2025-11-25 22:09:00 +0800

    help

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
