fc07dabf9c837e44eecf4a69aa05eac7e5f1940e
shshouse <1839192968@qq.com>
2025-12-07 15:35:15 +0800

    为所有歌曲标题添加中文翻译

--- src/data/songs.js
+++ src/data/songs.js
@@ -3,115 +3,115 @@ const R2_BASE_URL = 'https://studycdn.mikugame.icu'
 export const songsData = {
   part1: [
     { 
-      title: 'アスノヨゾラ哨戒班', 
+      title: '明日夜空巡逻班 / アスノヨゾラ哨戒班', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - アスノヨゾラ哨戒班 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ジャンキーナイトタウンオーケストラ', 
+      title: '瘾君子夜城管弦乐 / ジャンキーナイトタウンオーケストラ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ジャンキーナイトタウンオーケストラ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ロミオとシンデレラ', 
+      title: '罗密欧与灰姑娘 / ロミオとシンデレラ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ロミオとシンデレラ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: '雨とペトラ', 
+      title: '雨与佩特拉 / 雨とペトラ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - 雨とペトラ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: '少女レイ', 
+      title: '少女Rei / 少女レイ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - 少女レイ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: '恋愛裁判', 
+      title: '恋爱裁判 / 恋愛裁判', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - 恋愛裁判 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'potatoになっていく', 
+      title: '逐渐变成土豆 / potatoになっていく', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - potatoになっていく - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ごめんね ごめんね', 
+      title: '对不起 对不起 / ごめんね ごめんね', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ごめんね ごめんね - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'しわ', 
+      title: '皱纹 / しわ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - しわ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ダブルラリアット', 
+      title: '双重回旋臂 / ダブルラリアット', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ダブルラリアット - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ビターチョコデコレーション', 
+      title: '苦巧克力装饰 / ビターチョコデコレーション', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ビターチョコデコレーション - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ラグトレイン', 
+      title: '迟缓列车 / ラグトレイン', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ラグトレイン - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ラストリゾート', 
+      title: '最后的度假地 / ラストリゾート', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ラストリゾート - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'リモコン', 
+      title: '遥控器 / リモコン', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - リモコン - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ルーマー', 
+      title: '流言蜚语 / ルーマー', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ルーマー - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ヲズワルド', 
+      title: '奥兹华尔德 / ヲズワルド', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ヲズワルド - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: 'ヴァンパイア', 
+      title: '吸血鬼 / ヴァンパイア', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - ヴァンパイア - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: '贖罪', 
+      title: '赎罪 / 贖罪', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - 贖罪 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
     },
     { 
-      title: '転生林檎', 
+      title: '转生苹果 / 転生林檎', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part1/STUDY WITH MIKU - 転生林檎 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part1'
@@ -119,115 +119,115 @@ export const songsData = {
   ],
   part2: [
     { 
-      title: 'Awake now', 
+      title: '此刻觉醒 / Awake now', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - Awake now - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'さよならプリンセス', 
+      title: '再见公主 / さよならプリンセス', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - さよならプリンセス - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'ぼかろころしあむ', 
+      title: 'V家斗技场 / ぼかろころしあむ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - ぼかろころしあむ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'アンノウン・マザーグース', 
+      title: '未知的鹅妈妈 / アンノウン・マザーグース', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - アンノウン・マザーグース - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'インタビュア', 
+      title: '采访者 / インタビュア', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - インタビュア - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'エイリアンエイリアン', 
+      title: '外星人外星人 / エイリアンエイリアン', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - エイリアンエイリアン - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'ガランド', 
+      title: '空洞 / ガランド', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - ガランド - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'シャンティ', 
+      title: '香缇 / シャンティ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - シャンティ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'タイニーバニー', 
+      title: '小小兔子 / タイニーバニー', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - タイニーバニー - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'ド屑', 
+      title: '人渣 / ド屑', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - ド屑 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: 'ロウワー', 
+      title: '低处 / ロウワー', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - ロウワー - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: '千本桜', 
+      title: '千本樱 / 千本桜', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - 千本桜 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: '夜明けと蛍', 
+      title: '黎明与萤火虫 / 夜明けと蛍', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - 夜明けと蛍 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: '帝国少女', 
+      title: '帝国少女 / 帝国少女', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - 帝国少女 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: '幽霊東京', 
+      title: '幽灵东京 / 幽霊東京', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - 幽霊東京 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: '独りんぼエンヴィー', 
+      title: '孤独的嫉妒 / 独りんぼエンヴィー', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - 独りんぼエンヴィー - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: '猛独が襲う', 
+      title: '孤独来袭 / 猛独が襲う', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - 猛独が襲う - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: '生きる', 
+      title: '活着 / 生きる', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - 生きる - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
     },
     { 
-      title: '神っぽいな', 
+      title: '神气十足 / 神っぽいな', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part2/STUDY WITH MIKU - 神っぽいな - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part2'
@@ -235,115 +235,115 @@ export const songsData = {
   ],
   part3: [
     { 
-      title: '死んでしまったのだろうか', 
+      title: '是不是已经死了呢 / 死んでしまったのだろうか', 
       artist: 'Guiano', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/Guiano - 死んでしまったのだろうか - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'それがあなたの幸せとしても', 
+      title: '即使那是你的幸福 / それがあなたの幸せとしても', 
       artist: 'Heavenz', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/Heavenz - それがあなたの幸せとしても - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'ビバハピ', 
+      title: '万岁快乐 / ビバハピ', 
       artist: 'Mitchie M', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/Mitchie M - ビバハピ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'テオ', 
+      title: '提奥 / テオ', 
       artist: 'Omoi', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/Omoi - テオ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'Henceforth', 
+      title: '从此以后 / Henceforth', 
       artist: 'Orangestar & STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/Orangestar & STUDY WITH MIKU - Henceforth - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: '春嵐', 
+      title: '春岚 / 春嵐', 
       artist: 'john', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/john - 春嵐 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: '悪ノ娘', 
+      title: '恶之女儿 / 悪ノ娘', 
       artist: 'mothy & STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/mothy & STUDY WITH MIKU - 悪ノ娘 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'トンデモワンダーズ', 
+      title: '惊天奇想 / トンデモワンダーズ', 
       artist: 'sasakure.UK', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/sasakure.UK - トンデモワンダーズ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'きゅうくらりん', 
+      title: '晕头转向 / きゅうくらりん', 
       artist: 'いよわ', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/いよわ - きゅうくらりん - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: '夜咄ディセイブ', 
+      title: '夜话欺骗 / 夜咄ディセイブ', 
       artist: 'じん', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/じん - 夜咄ディセイブ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'エゴロック', 
+      title: '自我摇滚 / エゴロック', 
       artist: 'すりぃ', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/すりぃ - エゴロック - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: '我儘姫', 
+      title: '任性公主 / 我儘姫', 
       artist: 'ふじを', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/ふじを - 我儘姫 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'フォニイ', 
+      title: '虚伪 / フォニイ', 
       artist: 'ツミキ & STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/ツミキ & STUDY WITH MIKU - フォニイ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'ノンブレス・オブリージュ', 
+      title: '无呼吸义务 / ノンブレス・オブリージュ', 
       artist: 'ピノキオピー', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/ピノキオピー - ノンブレス・オブリージュ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'ブリキノダンス', 
+      title: '铁皮之舞 / ブリキノダンス', 
       artist: '日向電工', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/日向電工 - ブリキノダンス - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'マーシャル・マキシマイザー', 
+      title: '元帅最大化 / マーシャル・マキシマイザー', 
       artist: '柊マグネタイト', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/柊マグネタイト - マーシャル・マキシマイザー - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: '死んでしまったんだ', 
+      title: '已经死掉了 / 死んでしまったんだ', 
       artist: '椎乃味醂', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/椎乃味醂 - 死んでしまったんだ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'ロストアンブレラ', 
+      title: '遗失的雨伞 / ロストアンブレラ', 
       artist: '稲葉曇', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/稲葉曇 - ロストアンブレラ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
     },
     { 
-      title: 'シャルル', 
+      title: '夏尔 / シャルル', 
       artist: '須田景凪', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part3/須田景凪 - シャルル - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part3'
@@ -351,61 +351,61 @@ export const songsData = {
   ],
   partSEKAI: [
     { 
-      title: 'Be The MUSIC!', 
+      title: '化身音乐 / Be The MUSIC!', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - Be The MUSIC! - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: 'Journey', 
+      title: '旅途 / Journey', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - Journey - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: 'NEO', 
+      title: '新生 / NEO', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - NEO - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: 'Worlders', 
+      title: '世界居民 / Worlders', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - Worlders - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: 'アイムマイン', 
+      title: '我属于我 / アイムマイン', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - アイムマイン - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: 'セカイ', 
+      title: '世界 / セカイ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - セカイ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: 'ハローセカイ', 
+      title: '你好世界 / ハローセカイ', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - ハローセカイ - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: 'ワーワーワールド', 
+      title: '哇哇世界 / ワーワーワールド', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - ワーワーワールド - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: '熱風', 
+      title: '热风 / 熱風', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - 熱風 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
     },
     { 
-      title: '群青讃歌', 
+      title: '群青赞歌 / 群青讃歌', 
       artist: 'STUDY WITH MIKU', 
       src: `${R2_BASE_URL}/music/STUDY WITH MIKU part SEKAI/STUDY WITH MIKU - 群青讃歌 - STUDY WITH MIKU ver. -.mp3`,
       album: 'STUDY WITH MIKU part SEKAI'
